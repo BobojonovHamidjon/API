949d5c348ae51fce498091497f64022dba084cb3
hamidjon <163092495+BobojonovHamidjon@users.noreply.github.com>
2025-03-08 00:30:46 -1200

    firts commit

--- src/Login.jsx
+++ src/Login.jsx
@@ -2,6 +2,7 @@ import axios from "axios";
 import React from "react";
 import { useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 
 function Login() {
   const { register, handleSubmit } = useForm();
